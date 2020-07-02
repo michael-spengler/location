6cc6ffd14a698116026d488650cc5bad557c93a6
Michael Spengler <mycamper@gmx.de>
2020-07-02 20:33:59 +0200

    Introducing the city location service

---
+++ citylocationservice.ts
@@ -0,0 +1,19 @@
+
+import { CityService } from "https://deno.land/x/cities/cityservice.ts"
+
+export class CityLocationService {
+
+    public static async getCityLocation(countryCode: string, cityName: string): Promise<any> {
+
+        const cityInfo = CityService.getCityInfo(countryCode, cityName)
+
+        if (cityInfo === undefined) {
+            throw new Error(`I could not find any data for Country Code: ${countryCode} and City: ${cityName}`)
+        }
+
+        return {
+            latitude: cityInfo.lat,
+            longitude: cityInfo.lon,
+        }
+    }
+}
--- test-it.ts
+++ test-it.ts
@@ -1,7 +1,18 @@
 import { IPLocationService } from "./iplocationservice.ts"
+import { CityLocationService } from "./citylocationservice.ts"
 
 
 const ip = '49.12.6.159'
 const ipLocation = await IPLocationService.getIPLocation(ip)
 
-console.log(`I found the following location for ip ${ip}: \n${JSON.stringify(ipLocation)}\n`)
+console.log(`\nI found the following location for ip ${ip}: \n${JSON.stringify(ipLocation)}\n`)
+
+
+
+
+const countryCode = 'DE'
+const cityName = 'Heidelberg'
+const cityLocation = await CityLocationService.getCityLocation(countryCode, cityName)
+
+console.log(`The city you asked for is about here: \n${JSON.stringify(cityLocation)}\n`)
+
